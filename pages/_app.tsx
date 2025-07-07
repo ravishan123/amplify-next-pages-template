b2f6b4bf1249906eb206f8c04b961a457e6db59f
Amigo <66440787+ravishan123@users.noreply.github.com>
2025-07-07 23:26:44 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,8 @@ import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { Storage } from "@aws-amplify/storage";
 
 Amplify.configure(outputs);
-Amplify.register(Storage);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
